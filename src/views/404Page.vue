38cd1708fd2383cedabb39e54681a5d5b2a8d7f6
LinChinHsuan <hinolin0309@gmail.com>
2022-06-09 16:17:24 +0800

    修正404page的樣式

--- src/views/404Page.vue
+++ src/views/404Page.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="vh-100 bg-light d-flex flex-column align-items-center justify-content-center">
-    <h1 class="d-flex">
-      <span class="pb-2 me-2 fs-1 fw-bolder">找不到頁面</span>
-      <span class="fs-3 text-primary align-self-end">/ 404 Not Found</span>
+    <h1 class="d-flex mb-3 mb-md-4">
+      <span class="me-2 fw-bolder">找不到頁面</span>
+      <span class="text-primary align-self-end">/ 404 Not Found</span>
     </h1>
-    <p class="fs-5 mb-5">Oops！您要找的頁面被魔術師消失了</p>
+    <p class="mb-5">Oops！您要找的頁面被魔術師消失了</p>
     <RouterLink to="/" class="btn btn-outline-primary btn-lg fw-bold px-4 py-3">回到 找。牌 首頁</RouterLink>
   </div>
 </template>
 
 <style lang="scss" scoped>
+h1{
+  :first-child{
+    font-size: 2.5rem;
+    @media(max-width: 424px) {
+      font-size: 2rem;
+    }
+    @media(max-width: 374px) {
+      font-size: 1.75rem;
+    }
+  }
+  :last-child{
+    font-size: 1.75rem;
+    color: #587778;
+    @media(max-width: 424px) {
+      font-size: 1.25rem;
+    }
+    @media(max-width: 374px) {
+      font-size: 1rem;
+    }
+  }
+}
+p{
+  font-size: 1.25rem;
+  @media(max-width: 424px) {
+    font-size: 1rem;
+  }
+}
 .btn-outline-primary{
   transition: .5s;
   &:hover,
